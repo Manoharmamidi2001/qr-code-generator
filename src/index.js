f3a6848b33060c52d4a0b0c89ed10870fa2ba9c5
Mamidi Manohar <manoharmamidi2001@gmail.com>
2025-04-18 09:06:08 +0530

    route

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <App />
+  <HashRouter>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+    </Routes>
+  </HashRouter>
   </>
 );
